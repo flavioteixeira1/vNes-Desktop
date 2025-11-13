0780e2fe80f97cd490d014ba8ef2de3ec9c85fdd
flavioteixeira1 <flavioteixeira1@gmail.com>
2025-11-13 11:20:29 -0300

    bug fix Joystick

--- src/main/java/com/flavioteixeira1/vnes/core/JoystickConfig.java
+++ src/main/java/com/flavioteixeira1/vnes/core/JoystickConfig.java
@@ -1,6 +1,7 @@
 package com.flavioteixeira1.vnes.core;
 
 import java.util.prefs.Preferences;
+import java.awt.event.KeyEvent;
 
 public class JoystickConfig {
     private static final String PREFS_NODE = "com/vnes/joystick";
@@ -20,9 +21,9 @@ public class JoystickConfig {
     
     public void loadConfig(JoystickManager joystickManager) {
         // Carregar mapeamentos salvos
-       // joystickManager.setButtonMapping(0, loadButtonMapping(0, KeyEvent.VK_Z));
-       // joystickManager.setButtonMapping(1, loadButtonMapping(1, KeyEvent.VK_X));
-       // joystickManager.setButtonMapping(2, loadButtonMapping(2, KeyEvent.VK_ENTER));
-       // joystickManager.setButtonMapping(3, loadButtonMapping(3, KeyEvent.VK_CONTROL));
+        joystickManager.setButtonMapping(0, loadButtonMapping(0, KeyEvent.VK_Z));
+        joystickManager.setButtonMapping(1, loadButtonMapping(1, KeyEvent.VK_X));
+        joystickManager.setButtonMapping(2, loadButtonMapping(2, KeyEvent.VK_ENTER));
+        joystickManager.setButtonMapping(3, loadButtonMapping(3, KeyEvent.VK_CONTROL));
     }
 }
